c908e5a98a39069b2e4467e35de56a908abffd90
aryanmaurya-prog <mauryaaryan4910@gmail.com>
2025-09-11 01:32:04 +0530

    Added BarChart implementation

---
+++ src/MaxandMin.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MaxandMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
