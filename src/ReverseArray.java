c4953c0e9d81df0645c3ec223783da7edf21f710
aryanmaurya-prog <mauryaaryan4910@gmail.com>
2025-09-11 00:43:30 +0530

    Added BarChart implementation

---
+++ src/ReverseArray.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0;i< arr.length;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int i = 0;
+        int j = arr.length - 1;
+        int temp;
+
+        while (i<j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for(int k = 0;k<arr.length;k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
